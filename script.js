462faaa39b069225c02a85a5c05b5ef63b59cb3f
Abdullah khan <123239280+Abdullahkhan871@users.noreply.github.com>
2025-12-18 14:02:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-// Your code here.
+const items = document.querySelector('.items');
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+items.addEventListener('mousedown', (e) => {
+  isDown = true;
+  startX = e.pageX;
+  scrollLeft = items.scrollLeft;
+});
+
+document.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+
+  const x = e.pageX;
+  const walk = startX - x; // drag left → scroll right
+  items.scrollLeft = scrollLeft + walk;
+});
+
+document.addEventListener('mouseup', () => {
+  isDown = false;
+});
+
+
+
+
